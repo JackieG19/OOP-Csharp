c86d90c09c12a17b1377c7d30d480d936e756fb0
Jackie <jacgut19@gmail.com>
2018-05-29 14:41:17 +0000

    Lab

---
+++ Module-1/Lab/Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDXLabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+
+            Car1.Color = "Silver";
+            Car1.Year = 2004;
+            Car1.Mileage = 180000;
+
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles.");
+        }
+    }
+
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+    }
+}
---
+++ Module-2/Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2LabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiates Employee Object with name Libby and salary 2000 called employee1
+
+            var employee1 = new TechnicalEmployee("Libby");
+
+             //Instantiates TechincalEmployee Object with name Zaynah called employee2
+             var employee2 = new TechincalEmployee("Zaynah");
+
+              //Instantiates BusinessEmployee Object with name Winter called employee3
+              var employee3 = new BusinessEmployee("Winter");
+
+              Console.WriteLine(employee1.employeeStatus() + "..." + employee2.employeeStatus() + "..." + employee3.employeeStatus());
+        }
+    }    
+}
